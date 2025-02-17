85a9deac731caae0161b806a49179662f54a1ca2
nazisharifi2025 <banonazisharifi@gmail.com>
2025-02-17 14:42:52 +0430

    added naz

---
+++ app.js
@@ -0,0 +1,7 @@
+const bar = document.getElementById("bar");
+const ul= document.getElementById("ul");  
+if(bar){
+    bar.addEventListener('click',()=>{
+        header.classlist.add("a")
+    }); 
+}
