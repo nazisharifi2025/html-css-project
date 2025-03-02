2fd44f9d62f14bb53b471278fc228c98a12f5dba
nazisharifi2025 <banonazisharifi@gmail.com>
2025-03-02 20:02:35 +0430

    added allas

---
+++ src/app.js
@@ -0,0 +1,11 @@
+// function user(num1,nume2){
+//     let result =num1+nume2;
+//     return result;
+// }
+// console.log(user(9,5));
+// alert(user(4,9));
+function number(number1,number2,number3,number4){
+    let result=number1,number2,number3,number4;
+    return result;
+}
+console.log(number(8,10,12,5));
