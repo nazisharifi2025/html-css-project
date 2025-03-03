4036f3b66b6f68b00b8e25c0efa6d2b99d7c75e3
nazisharifi2025 <banonazisharifi@gmail.com>
2025-03-03 20:47:58 +0430

    added js

--- src/app.js
+++ src/app.js
@@ -1,35 +1,18 @@
-// function user(num1,nume2){
-//     let result =num1+nume2;
-//     return result;
-// }
-// console.log(user(9,5));
-// alert(user(4,9));
-// function number(number1,number2,){
-//     let result=number1+number2;
-//     return result;
-// }
-// console.log(number(8,10,));
-// alert(number(12,90))
-// function contusion(n1,n2){
-//     let result =n1*n2;
-//     return result;
-// }
-// console.log(contusion(120,20));
-// // hoisting
-// let result;
-// function division(num12,num13){
-//     result=num12/num13;
 
-// }
-// division(230,12)
-// console.log(result)
-// function subtraction(num0,num9){
-//     let result=num0-num9;
-//     return result;
-// }
-// alert(subtraction(10,1));
-// function run(number5,number6){
-//     let result=number5%number6;
-//     return result;
-// }
-// console.log(run(12,12))
+const add =document.getElementById("plus");
+const minus=document.getElementById("minus");
+const divide=document.getElementById("divide");
+const module=document.getElementById("module");
+const tiems=document.getElementById("tiems");
+const number=document.getElementById("number");
+const result=document.getElementById("result")
+
+let DefaultResult=0;
+function addtion(){
+    DefaultResult= DefaultResult+parseInt(number.value);
+    showResult(DefaultResult);
+}
+function showResult(DefaultResult) {
+    result.textContent =DefaultResult;
+}
+add.addEventListener('click',addtion)
