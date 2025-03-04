cbf8f79b3f43771970bcd7a3be60ee9984c5574a
nazisharifi2025 <banonazisharifi@gmail.com>
2025-03-04 21:16:01 +0430

    add respansiv

--- exarsize.js
+++ exarsize.js
@@ -1,6 +1,1 @@
-let people=[
-    {"name":'ali',"lastName":'sharifi',"age":18}
-    {"name":"nazi","lastName":"hadi","age":19}
-    {"name":'nazifa',"lastName":'hadisharifi',"age":20}
-]
-console.log(computer.RamSize);
+
