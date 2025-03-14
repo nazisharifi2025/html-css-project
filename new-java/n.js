83c3cec946eacda5eb54034551113c23e20245c3
nazisharifi2025 <banonazisharifi@gmail.com>
2025-03-14 19:55:39 +0430

    added j-n

---
+++ new-java/n.js
@@ -0,0 +1,18 @@
+let s = 9;
+switch(s){
+    case 3 :
+    console.log("nine");
+    break;
+    case 6 :
+    console.log("six");
+    break ;
+    case 8:
+        console.log("egth");
+        break;
+        case 9:
+            console.log("nine");
+            break;
+            default:
+                console.log("no isent");
+                break;
+}
