d04ec123fff72b96c49db4185914cdd635abdc83
nazisharifi2025 <banonazisharifi@gmail.com>
2025-03-14 20:09:56 +0430

    added new

--- new-java/n.js
+++ new-java/n.js
@@ -16,3 +16,31 @@ switch(s){
                 console.log("no isent");
                 break;
 }
+let date = new date().getday();
+switch(date){
+    case 1:
+        console.log("Monday");
+        break;
+        case 2:
+            console.log("Tuesday");
+            break;
+            case 3:
+            console.log("Wednesday");
+            break;
+            case 4:
+                console.log("Fersday");
+                break;
+            case 5:
+                console.log("Friday");
+                break;
+             case 6 :
+                console.log ("Horray today is the end of week,");
+                break;
+                case 7:
+                    console.log("Sunday");
+                    break; 
+                    default:
+                        console.log("Tise is not the valid day number");
+                        break;   
+
+}
