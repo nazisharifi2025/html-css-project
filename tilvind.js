ca761f2b4968f1ef4a8c2ff400d38871bdaf3619
nazisharifi2025 <banonazisharifi@gmail.com>
2025-04-03 15:49:53 +0430

    l

--- tilvind.js
+++ tilvind.js
@@ -1,33 +1,36 @@
-let currentTime = new Date();
+const hrs = document.getElementById("hov");
+const min = document.getElementById("min");
+const sec = document.getElementById("sco");
+
+let currentTime = new Data();
 let hours = currentTime.getHours();
-let minutes = currentTime.getMinutes();
-let second = currentTime.getSeconds();
+
 
 function Date(){
     
 }
-let s=[1,2,3,23,12,45,54];
-console.log(s);
-if(length.s===6){
-    console.log("hi naz");
-}
-else if(length.s===5){
-    console.log("thes is 5");
-}
-else if(length.s===4){
-    console.log("thes is 4");
-}
-else if(length.s===7){
-    console.log("thes is 7");
-}
-else{
-    console.log("no it is.")
-}
-let f =["naz",18,"sharifa",23,true,
-    {
-        name1:"naz",
-        lastName:"sharifi",
-        age:"18",
-    }
-];
-console.log(f);
+// let s=[1,2,3,23,12,45,54];
+// console.log(s);
+// if(length.s===6){
+//     console.log("hi naz");
+// }
+// else if(length.s===5){
+//     console.log("thes is 5");
+// }
+// else if(length.s===4){
+//     console.log("thes is 4");
+// }
+// else if(length.s===7){
+//     console.log("thes is 7");
+// }
+// else{
+//     console.log("no it is.")
+// }
+// let f =["naz",18,"sharifa",23,true,
+//     {
+//         name1:"naz",
+//         lastName:"sharifi",
+//         age:"18",
+//     }
+// ];
+// console.log(f);
